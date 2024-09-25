98f9447479b7bef44cfac83704dcd5ad2667bc61
meustar <meustar829@gmail.com>
2024-09-25 23:59:45 +0900

    MemberController 도입, 기능 이전

---
+++ src/main/java/org/example/MemberController.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MemberController {
+
+    Scanner sc;
+    List<Member> members;
+
+    private int lastMemberId = 3;
+
+    public MemberController(Scanner sc) {
+        this.sc = sc;
+        members = new ArrayList<>();
+    }
+
+    public void dojoin() {
+        System.out.println("== 회원 가입==");
+        int id = lastMemberId + 1;
+        String regDate = Util.getNow();
+
+        String loginId = null;
+        while (true) {
+            System.out.print("로그인 아이디 : ");
+            loginId = sc.nextLine().trim();
+            if (isjoinableLoginId(loginId) == false) {
+                System.out.println("이미 사용중인 아이디 입니다.");
+                continue;
+            }
+            break;
+        }
+        String loginPw = null;
+        while (true) {
+            System.out.print("비밀번호 : ");
+            loginPw = sc.nextLine();
+            System.out.print("비밀번호 확인 : ");
+            String loginPwConfirm = sc.nextLine();
+
+            if (loginPw.equals(loginPwConfirm) == false) {
+                System.out.println("비밀번호를 다시 확인해주세요.");
+                continue;
+            }
+            break;
+        }
+        System.out.print("이름 : ");
+        String name = sc.nextLine();
+
+        Member member = new Member(id, regDate, loginId, loginPw, name);
+        members.add(member);
+
+        System.out.println(id + "번 회원이 가입되었습니다.");
+        lastMemberId++;
+    }
+
+    private boolean isjoinableLoginId(String loginId) {
+        for (Member member : members) {
+            if(member.getLoginId().equals(loginId)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void makeTestData() {
+        System.out.println("회원 테스트 데이터 생성");
+        members.add(new Member(1, Util.getNow(), "test1", "test1", "test1"));
+        members.add(new Member(2, Util.getNow(), "test2", "test2", "test2"));
+        members.add(new Member(3, Util.getNow(), "test3", "test3", "test3"));
+    }
+
+}
