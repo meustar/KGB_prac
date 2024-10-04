a1672cefdf086c365d92d59cc99ae9a630706afa
meustar <meustar829@gmail.com>
2024-10-04 22:45:41 +0900

    Dao, Container 추가, list에 작성자 이름 보이도록 변경

--- src/main/java/org/example/controller/ArticleController.java
+++ src/main/java/org/example/controller/ArticleController.java
@@ -1,6 +1,8 @@
 package org.example.controller;
 
+import org.example.articleManager.Container;
 import org.example.dto.Article;
+import org.example.dto.Member;
 import org.example.util.Util;
 
 import java.util.ArrayList;
@@ -15,9 +17,11 @@ public class ArticleController extends Controller {
 
     private int lastArticle = 3;
 
+    List<Member> members = Container.memberDao.members;
+
     public ArticleController(Scanner sc) {
         this.sc = sc;
-        articles = new ArrayList<>();
+        articles = Container.articleDao.articles;
     }
 
     public void doAction(String cmd, String actionMethodName) {
@@ -90,13 +94,23 @@ public class ArticleController extends Controller {
             }
         }
 
+        String writerName = null;
+
         System.out.println("   번호   /      날짜      /   작성자   /   제목   /   내용");
         for (int i = forPrintArticles.size() - 1; i >= 0; i--) {
             Article article = forPrintArticles.get(i);
+
+            for(Member member : members) {
+                if(article.getMemberId() == member.getId()) {
+                    writerName = member.getName();
+                    break;
+                }
+            }
+
             if (Util.getNow().split(" ")[0].equals(article.getRegDate().split(" ")[0])) { // 같은 날이라면.
-                System.out.printf("   %d   /      %s     /     %d     /     %s     /     %s   \n", article.getId(), article.getRegDate().split(" ")[1], article.getMemberId(), article.getTitle(), article.getBody());
+                System.out.printf("   %d   /      %s     /     %s     /     %s     /     %s   \n", article.getId(), article.getRegDate().split(" ")[1], writerName, article.getTitle(), article.getBody());
             } else {
-                System.out.printf("   %d   /      %s     /     %d     /     %s     /     %s   \n", article.getId(), article.getRegDate().split(" ")[0], article.getMemberId(), article.getTitle(), article.getBody());
+                System.out.printf("   %d   /      %s     /     %s     /     %s     /     %s   \n", article.getId(), article.getRegDate().split(" ")[0], writerName, article.getTitle(), article.getBody());
             }
         }
     }
--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -2,9 +2,13 @@ package org.example.controller;
 
 import org.example.dto.Member;
 
+import java.util.List;
+
 public abstract class Controller {
 
     protected static Member loginedMember = null;
+
+    protected static List<Member> members;
 
     public abstract void doAction(String cmd, String actionMethodName);
 
