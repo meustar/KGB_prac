251981b65908ffa2523acbdc2e9d791c2e6d5b8a
meustar <meustar829@gmail.com>
2024-10-10 02:59:20 +0900

    Service 구조 도입 완료

--- src/main/java/org/koreait/motivation/controller/MotivationController.java
+++ src/main/java/org/koreait/motivation/controller/MotivationController.java
@@ -2,7 +2,6 @@ package org.koreait.motivation.controller;
 
 import org.koreait.Container;
 import org.koreait.Rq;
-import org.koreait.motivation.entity.Motivation;
 import org.koreait.motivation.service.MotivationService;
 
 public class MotivationController {
@@ -21,93 +20,44 @@ public class MotivationController {
         System.out.print("body : ");
         String body = Container.getScanner().nextLine();
 
-        int id = motivationService.add(source, body);
+        int id = motivationService.doAdd(source, body);
 
         System.out.println(id + "번 motivation이 등록 되었습니다.");
 
     }
 
     public void list() {
-        if (motivations.isEmpty()) {
-            System.out.println("등록된 motivation이 없습니다.");
-            return;
-        }
-        System.out.println(" == motivation list == ");
-        System.out.print("   id   //        source        //              body            \n");
-        System.out.println("=".repeat(50));
-
-        for (int i = motivations.size() - 1; i >= 0; i--) {
-            Motivation motivation = motivations.get(i);
-
-            if (motivation.getSource().length() > 7) {
-                System.out.printf("    %d   //       %s       //        %s          \n", motivation.getId(), motivation.getSource().substring(0, 5) + "...", motivation.getBody());
-                continue;
-            }
-
-            System.out.printf("    %d   //       %s       //        %s          \n", motivation.getId(), motivation.getSource(), motivation.getBody());
-        }
+        motivationService.showList();
     }
 
     public void delete(Rq rq) {
         System.out.println(" == motivation delete == ");
-        int id;
-        try {
-            id = Integer.parseInt(rq.getParams("id"));
-        } catch (NumberFormatException e ) {
+
+        int id = motivationService.doDelete(rq);
+
+        if ( id == -1) {
             System.out.println("정수 입력 오류");
             return;
-        }
-
-        Motivation motivation = findById(id);
-
-        if ( motivation == null) {
-            System.out.println(id + "번 motivation은 없습니다.");
+        } else if (id == -2) {
+            System.out.println("해당 motivation은 없습니다.");
             return;
         }
 
-        motivations.remove(motivation);
         System.out.println(id + "번 motivation을 삭제했습니다.");
     }
 
     public void edit(Rq rq) {
         System.out.println(" == motivation edit == ");
-        int id;
-        try {
-            id = Integer.parseInt(rq.getParams("id"));
-        } catch (NumberFormatException e ) {
+
+        int id = motivationService.doEdit(rq);
+
+        if ( id == -1) {
             System.out.println("정수 입력 오류");
             return;
-        }
-
-        Motivation motivation = findById(id);
-
-        if ( motivation == null) {
-            System.out.println(id + "번 motivation은 없습니다.");
+        } else if (id == -2) {
+            System.out.println("해당 motivation은 없습니다.");
             return;
         }
-        // 불러온 motivation의 인스턴스 변수에 접근.
-        System.out.println("source : " + motivation.getSource());
-        System.out.println("body : " + motivation.getBody());
-
-        System.out.print("new source : ");
-        String source = Container.getScanner().nextLine();
-        System.out.print("new body : ");
-        String body = Container.getScanner().nextLine();
-
-        // motivation의 인스턴스변수 수정.
-        motivation.setSource(source);
-        motivation.setBody(body);
-
         System.out.println(id + "번 motivation을 수정했습니다.");
-    }
-
-    // 입력된 id와 일치하는 motivation 찾기
-    private Motivation findById(int id) {
-        for (Motivation motivation : motivations) {
-            if(motivation.getId() == id) {
-                return motivation;
-            }
-        }
-        return null;
     }
 }
--- src/main/java/org/koreait/motivation/service/MotivationService.java
+++ src/main/java/org/koreait/motivation/service/MotivationService.java
@@ -1,5 +1,7 @@
 package org.koreait.motivation.service;
 
+import org.koreait.Container;
+import org.koreait.Rq;
 import org.koreait.motivation.entity.Motivation;
 
 import java.util.ArrayList;
@@ -15,9 +17,97 @@ public class MotivationService {
         motivations = new ArrayList<>();
     }
 
-    public int add(String source, String body) {
+    public int doAdd(String source, String body) {
         int id = lastMotivationId + 1;
 
         Motivation motivation = new Motivation(id, source, body);
+        motivations.add(motivation);
+
+        lastMotivationId = id;
+        return id;
+    }
+
+    public void showList() {
+        if (motivations.isEmpty()) {
+            System.out.println("등록된 motivation이 없습니다.");
+            return;
+        }
+        System.out.println(" == motivation list == ");
+        System.out.print("   id   //        source        //              body            \n");
+        System.out.println("=".repeat(50));
+
+        for (int i = motivations.size() - 1; i >= 0; i--) {
+            Motivation motivation = motivations.get(i);
+
+            if (motivation.getSource().length() > 7) {
+                System.out.printf("    %d   //       %s       //        %s          \n", motivation.getId(), motivation.getSource().substring(0, 5) + "...", motivation.getBody());
+                continue;
+            }
+
+            System.out.printf("    %d   //       %s       //        %s          \n", motivation.getId(), motivation.getSource(), motivation.getBody());
+        }
+    }
+
+    public int doDelete(Rq rq) {
+        int id;
+
+        try {
+            id = Integer.parseInt(rq.getParams("id"));
+        } catch (NumberFormatException e ) {
+            System.out.println("정수 입력 오류");
+            return -1;
+        }
+
+        Motivation motivation = findById(id);
+
+        if ( motivation == null) {
+            System.out.println(id + "번 motivation은 없습니다.");
+            return -2;
+        }
+
+        motivations.remove(motivation);
+
+        return id;
+    }
+
+    public int doEdit(Rq rq) {
+        int id;
+        try {
+            id = Integer.parseInt(rq.getParams("id"));
+        } catch (NumberFormatException e ) {
+
+            return -1;
+        }
+
+        Motivation motivation = findById(id);
+
+        if ( motivation == null) {
+            System.out.println(id + "번 motivation은 없습니다.");
+            return -2;
+        }
+        // 불러온 motivation의 인스턴스 변수에 접근.
+        System.out.println("source : " + motivation.getSource());
+        System.out.println("body : " + motivation.getBody());
+
+        System.out.print("new source : ");
+        String source = Container.getScanner().nextLine();
+        System.out.print("new body : ");
+        String body = Container.getScanner().nextLine();
+
+        // motivation의 인스턴스변수 수정.
+        motivation.setSource(source);
+        motivation.setBody(body);
+
+        return id;
+    }
+
+    // 입력된 id와 일치하는 motivation 찾기
+    private Motivation findById(int id) {
+        for (Motivation motivation : motivations) {
+            if(motivation.getId() == id) {
+                return motivation;
+            }
+        }
+        return null;
     }
 }
